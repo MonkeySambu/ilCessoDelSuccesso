4e9a335695db5965b40fda208ab91dc4163b027a
MonkeySambu <sambu88official@outlook.it>
2023-12-09 08:47:01 +0100

    fix login

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -1,11 +1,16 @@
-/*$(document).load(function(){
-    $("#err").hide;
-});*/
-$("#invia").click(function(){
-    if(($("#pass").val()=="Admin1234")&&($("#nome").val()=="ADMIN")){
-        $(location).prop('href', 'http://www.google.com')}
-    else{
-        $("#pass,#nome").addClass("rosso");
-        $("#err").hide;}
-        console.log("prova");
-    });
+$(document).ready(function(){
+    $("#invia").click(function(){
+        if(($("#pass").val()=="Admin1234")&&($("#nome").val()=="ADMIN")){
+            $(location).prop('href', 'negozio.html')}
+        else{
+            $("#pass,#nome").addClass("rosso");
+            $("#err").css({"display":"block"});
+            $("#spazio").css({"display":"none"});}
+        });
+})
+
+$(document).ready(function(){
+    
+    $("#logout").click(function(){
+        $(location).prop('href', 'index.html')})
+})
