02d8d8eac3c5fc5141c1eecf88d5ce5e83ee39e9
MonkeySambu <sambu88official@outlook.it>
2023-12-14 13:48:03 +0100

    COOKIEEEE

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -1,13 +1,31 @@
+var CookiesNome = Cookies.noConflict();
+var log=0;
+
+if(CookiesNome.get("nome")=="true" && CookiesNome.get("pass")=="true" && log!=0){
+    $(location).prop('href', 'negozio.html');
+    log=1;
+}
+
 $(document).ready(function(){
     $("#invia").click(function(){
         if(($("#pass").val()=="Admin1234")&&($("#nome").val()=="ADMIN")){
+
+            CookiesNome.set("nome","true",{expires: 1, sameSite: 'strict'});
+            CookiesNome.set("pass","true",{expires: 1,sameSite: 'strict'});
+            log=1;
             $(location).prop('href', 'negozio.html')}
+
         else{
             $("#pass,#nome").addClass("rosso");
             $("#err").css({"display":"block"});
             $("#spazio").css({"display":"none"});}
         });
 })
+
+if(CookiesNome.get("pass") != "true"){
+    window.location.replace("index.html");
+    log=0;
+}
 
 $(document).ready(function(){
     $("#logout").click(function(){
