9773569efd101f6bc16f533a76ca9375660ad9c6
MonkeySambu <sambu88official@outlook.it>
2023-12-16 11:49:35 +0100

    AAAAAAAAAAAAAA
    aSASDASDSAD
    
    Co-Authored-By: Cateee2 <129735347+Cateee2@users.noreply.github.com>

--- JavaScript/negozio.js
+++ JavaScript/negozio.js
@@ -1,5 +1,6 @@
-var CookiesProdotto= Cookies.noConflict();
+var CookiesProdotto= CookiesNome.noConflict();
 
+var e=0;
 $(document).ready(function(){
     for (let i = 0; i < 10; i++){
         $("#add"+i).click(function(){
@@ -16,6 +17,17 @@ $(document).ready(function(){
             CookiesProdotto.set("quantita"+i,quantita,{expires: 1, sameSite: 'strict'});
             CookiesProdotto.set("totale"+i,totale,{expires: 1, sameSite: 'strict'});
 
+            CookiesProdotto.get("foto"+i);
         });
     }
+
+    $("#cart").click(function(){
+        if(e==0){
+            $("#CARRELLO-PROD").show();
+            e=1;
+        }else{
+            $("#CARRELLO-PROD").hide();
+            e=0;
+        }
+    });
 });
