c64344bfe09bd2e3f341c8698c0c32a869022c47
MonkeySambu <113924005+MonkeySambu@users.noreply.github.com>
2023-12-21 11:44:03 +0000

    Add color change animation to labels and h2 elements, and update styling in negozio.css and negozio.html

--- JavaScript/negozio.js
+++ JavaScript/negozio.js
@@ -25,10 +25,11 @@ $(document).ready(function(){
             CookiesProdotto.get("nome"+i);
             CookiesProdotto.get("prezzo"+i);
             CookiesProdotto.get("quantita"+i);
-            var immagine = $("<img>").attr("src", CookiesProdotto.get("foto"+i));
-            $("#carrelloso").append("<tr><td>"+immagine+"</td><td>"+CookiesProdotto.get("quantita"+i)+"</td></tr>");
-
-            console.log(CookiesProdotto.get("nome"+i));
+            
+            var immagine = $("<img>");
+            var img = CookiesProdotto.get("foto"+i);
+            immagine.attr("src", img);
+            immagine.addClass("prod");
 
             var subtotale=parseFloat(CookiesProdotto.get("prezzo"+i))*parseFloat(CookiesProdotto.get("quantita"+i));
             console.log(subtotale);
@@ -36,9 +37,31 @@ $(document).ready(function(){
             CookiesProdotto.get("subtotale"+i);
             console.log(CookiesProdotto.get("subtotale"+i));
 
+            var cella1= $("<td> </td>").append(immagine);
+            var cella2= $("<td> </td>").append("<div>"+CookiesProdotto.get("nome"+i)+"</div>"+"<div class="+"fianco"+">Prezzo x Art: "
+                                                +CookiesProdotto.get("prezzo"+i)+" €</div>");
+            var cella3= $("<td> </td>").append("<div> Numero Art: "+CookiesProdotto.get("quantita"+i)+"</div>"+"<div class="+"fianco"+">subtotale: "+
+                                                CookiesProdotto.get("subtotale"+i)+" €"+"</div>"+"<br>"+"<button id='remove"+i+"'>Rimuovi</button>");
+            var riga= $("<tr> </tr>").append(cella1,cella2,cella3).attr("id","rigaCart"+i);
+            var rigaCookie= $("#rigaCart"+i).;
+            CookiesProdotto.set("riga"+i,rigaCookie,{expires: 1, sameSite: 'strict'});
+            
+            $("#carrelloso").prepend(CookiesProdotto.get("riga"+i));
+            
+           
+            console.log(CookiesProdotto.get("nome"+i));
+
+           
             totale += parseFloat(CookiesProdotto.get("subtotale"+i));
             $("#totale").html("Totale: "+totale+" €");
+
+            $("#remove"+i).click(function(){
+                console.log("test remove");
+                $("#rigaCart"+i).remove();
+                totale -= parseFloat(CookiesProdotto.get("subtotale"+i));
+                $("#totale").html("Totale: "+totale+" €");
             });
+        });
         }
 
         $("#cart").click(function(){
@@ -52,4 +75,6 @@ $(document).ready(function(){
                 e=0;
             }
         });
+
+        
     });
