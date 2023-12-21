c33dcadd35deaf1b21bdb77e3997d9a172927af0
MonkeySambu <113924005+MonkeySambu@users.noreply.github.com>
2023-12-21 21:33:33 +0000

    DIO CANE non va un cazzo Co-authored-by: Cateee2 <Cateee2@users.noreply.github.com>

--- JavaScript/negozio.js
+++ JavaScript/negozio.js
@@ -1,81 +1,86 @@
 var CookiesProdotto= CookiesNome.noConflict();
 var e=0;
-var totale=0;
-var numProd=0;
+var numProd=[];
 
 const PROD=11;
+
 $(document).ready(function(){
-    
-});
-$(document).ready(function(){
-    for (let i = 0; i <= CookiesProdotto.get("numProd"); i++){
-        var immagine = $("<img>");
-        var img = CookiesProdotto.get("foto"+i);
-        immagine.attr("src", img);
-        immagine.addClass("prod");
-    
-        var cella1= $("<td> </td>").append(immagine);
-        var cella2= $("<td> </td>").append("<div>"+CookiesProdotto.get("nome"+i)+"</div>"+"<div class="+"fianco"+">Prezzo x Art: "
-                                            +CookiesProdotto.get("prezzo"+i)+" €</div>");
-        var cella3= $("<td> </td>").append("<div> Numero Art: "+CookiesProdotto.get("quantita"+i)+"</div>"+"<div class="+"fianco"+">subtotale: "+
-                                            CookiesProdotto.get("subtotale"+i)+" €"+"</div>"+"<br>"+"<button id='remove"+i+"'>Rimuovi</button>");
-        var riga= $("<tr> </tr>").append(cella1,cella2,cella3).attr("id","rigaCart"+i);
-        $("#totale").html("Totale: "+CookiesProdotto.get("totale")+" €");
-        $("#carrelloso").prepend(riga);
-        }
-        
     for (let i = 0; i < PROD; i++){
         $("#add"+i).click(function(){
+            numProd=CookiesProdotto.get("numProd");
             numProd++;
             // VARIBALILI 
             var foto=$("#foto"+i).attr("src");
             var nome=$("#nome"+i).text();
             var prezzo=$("#prezzo"+i).text();
-            var quantita= parseInt($("#quantita"+i).val());
+            var quantita=parseInt($("#quantita"+i).val());
 
-            CookiesProdotto.set("foto"+i ,foto,{expires: 1, sameSite: 'strict'});
-            CookiesProdotto.set("nome"+i ,nome,{expires: 1, sameSite: 'strict'});
-            CookiesProdotto.set("prezzo"+i ,prezzo,{expires: 1, sameSite: 'strict'});
-            CookiesProdotto.set("quantita"+i ,quantita,{expires: 1, sameSite: 'strict'});
+            var check= CookiesProdotto.get("add"+i);
+            var cazzo= CookiesProdotto.get("quantita"+i);
+            console.log(check);
+            console.log(quantita);
+            console.log(cazzo);
+            if(check==true){
+                quantita = ((parseInt(cazzo)*100) + (quantita*100))/100;
+            }
+            console.log(quantita);
 
-            CookiesProdotto.set("numProd",numProd,{expires: 1, sameSite: 'strict'});
+            if(quantita==0){
+                alert("Inserire un numero di articoli!!!!!");
+            }else{
+                CookiesProdotto.set("foto"+i ,foto,{expires: 1, sameSite: 'strict'});
+                CookiesProdotto.set("nome"+i ,nome,{expires: 1, sameSite: 'strict'});
+                CookiesProdotto.set("prezzo"+i ,prezzo,{expires: 1, sameSite: 'strict'});
+                CookiesProdotto.set("quantita"+i ,quantita,{expires: 1, sameSite: 'strict'});
+                CookiesProdotto.set("posArt"+i,i,{expires: 1, sameSite: 'strict'});
+
+                CookiesProdotto.set("add"+i,true,{expires: 1, sameSite: 'strict'});
+                
+                var immagine = $("<img>");
+                var img = CookiesProdotto.get("foto"+i);
+                immagine.attr("src", img);
+                immagine.addClass("prod");
+
+
+                var subtotale=parseFloat(CookiesProdotto.get("prezzo"+i))*parseFloat(CookiesProdotto.get("quantita"+i));
+                CookiesProdotto.set("subtotale"+i,subtotale,{expires: 1, sameSite: 'strict'});
+
+                $("#rigaCart"+i).remove();
+                var cella1= $("<td> </td>").append(immagine);
+                var cella2= $("<td> </td>").append("<div>"+CookiesProdotto.get("nome"+i)+"</div>"+"<div class="+"fianco"+">Prezzo x Art: "+CookiesProdotto.get("prezzo"+i)+" €</div>");
+                var cella3= $("<td> </td>").append("<div> Numero Art: "+CookiesProdotto.get("quantita"+i)+"</div>"+"<div class="+"fianco"+">subtotale: "+CookiesProdotto.get("subtotale"+i)+" €"+"</div>"+"<br>"+"<button id='remove"+i+"'>Rimuovi</button>");
+                var riga= $("<tr> </tr>").append(cella1,cella2,cella3).attr("id","rigaCart"+i);
             
-            var immagine = $("<img>");
-            var img = CookiesProdotto.get("foto"+i);
-            immagine.attr("src", img);
-            immagine.addClass("prod");
+                $("#carrelloso").prepend(riga);
+                
+                totale += parseFloat(CookiesProdotto.get("subtotale"+i));
 
-            var subtotale=parseFloat(CookiesProdotto.get("prezzo"+i))*parseFloat(CookiesProdotto.get("quantita"+i));
-            CookiesProdotto.set("subtotale"+i,subtotale,{expires: 1, sameSite: 'strict'});
-
-            var cella1= $("<td> </td>").append(immagine);
-            var cella2= $("<td> </td>").append("<div>"+CookiesProdotto.get("nome"+i)+"</div>"+"<div class="+"fianco"+">Prezzo x Art: "
-                                                +CookiesProdotto.get("prezzo"+i)+" €</div>");
-            var cella3= $("<td> </td>").append("<div> Numero Art: "+CookiesProdotto.get("quantita"+i)+"</div>"+"<div class="+"fianco"+">subtotale: "+
-                                                CookiesProdotto.get("subtotale"+i)+" €"+"</div>"+"<br>"+"<button id='remove"+i+"'>Rimuovi</button>");
-            var riga= $("<tr> </tr>").append(cella1,cella2,cella3).attr("id","rigaCart"+i);
-            
-            $("#carrelloso").prepend(riga);
-            
-           
-            console.log(CookiesProdotto.get("nome"+i));
-
-           
-            totale += parseFloat(CookiesProdotto.get("subtotale"+i))+parseFloat(CookiesProdotto.get("totale"));
-            totale = totale.toFixed(2);
-            CookiesProdotto.set("totale",totale,{expires: 1, sameSite: 'strict'});
-            $("#totale").html("Totale: "+totale+" €");
+                CookiesProdotto.set("totale",totale,{expires: 1, sameSite: 'strict'});
+                $("#totale").html("Totale: "+totale+" €");
+            }
 
             $("#remove"+i).click(function(){
                 console.log("test remove");
                 $("#rigaCart"+i).remove();
+                CookiesProdotto.set("add"+i,false,{expires: 1, sameSite: 'strict'});
+                CookiesProdotto.remove("foto"+i);
+                CookiesProdotto.remove("nome"+i);
+                CookiesProdotto.remove("prezzo"+i);
+                CookiesProdotto.remove("quantita"+i);
+                CookiesProdotto.remove("subtotale"+i);
+                CookiesProdotto.remove("posArt"+i);
+
                 totale -= parseFloat(CookiesProdotto.get("subtotale"+i));
                 totale = totale.toFixed(2);
                 CookiesProdotto.set("totale",totale,{expires: 1, sameSite: 'strict'});
                 $("#totale").html("Totale: "+totale+" €");
             });
+
+
         });
         }
+
+
 
         $("#cart").click(function(){
             if(e==0){
@@ -91,3 +96,23 @@ $(document).ready(function(){
 
         
     });
+    $("#totale").remove();
+$(document).ready(function(){
+    for (let i = 0; i < PROD; i++){
+                
+        setTimeout(function(){
+            if(CookiesProdotto.get("posArt"+i)==i){
+                var immagine = $("<img>");
+                var img = CookiesProdotto.get("foto"+i);
+                immagine.attr("src", img);
+                immagine.addClass("prod");
+                
+                var cella1= $("<td> </td>").append(immagine);
+                var cella2= $("<td> </td>").append("<div>"+CookiesProdotto.get("nome"+i)+"</div>"+"<div class="+"fianco"+">Prezzo x Art: "+CookiesProdotto.get("prezzo"+i)+" €</div>");
+                var cella3= $("<td> </td>").append("<div> Numero Art: "+CookiesProdotto.get("quantita"+i)+"</div>"+"<div class="+"fianco"+">subtotale: "+CookiesProdotto.get("subtotale"+i)+" €"+"</div>"+"<br>"+"<button id='remove"+i+"'>Rimuovi</button>");
+                var riga= $("<tr> </tr>").append(cella1,cella2,cella3).attr("id","rigaCart"+i);
+                $("#totale").html("Totale: "+CookiesProdotto.get("totale")+" €");
+                $("#carrelloso").prepend(riga);
+                }
+            },1000);
+    }});
